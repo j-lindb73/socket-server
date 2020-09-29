b6cbc7061bf8f46ca85dcae3a47cf55c81b3e811
Johan Lindberg <johan@hasselstigen.se>
2020-09-29 16:42:26 +0100

    Added CORS hasselstige.me 2

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const port = 3000;
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-io.origins(['https://*.hasselstigen.me:443']);
+// io.origins(['https://*.hasselstigen.me:443']);
+io.set('origins', ['https://socket-client.hasselstigen.me:443', 'https://me-app.hasselstigen.me:443']);
 
 io.on('connection', function (socket) {
     console.log("User connected");
